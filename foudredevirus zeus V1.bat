1e7f381327e60e74ce3a2c1853dc89143fa78eeb
dassored <101705817+dassored@users.noreply.github.com>
2023-03-21 16:49:13 +0100

    fake alerte virus foudre de zeus !

---
+++ foudredevirus zeus V1.bat
@@ -0,0 +1,31 @@
+mode CON: COLS=700
+color 4
+@echo off
+echo -----------------------------------------------------------------------------------------------------------
+echo waring windose10:
+echo warning error 404 , warning : Matériel défectueux Page corrompue : 0x0000012B , warning error windose  0xc0000005, Euror 1X00000 Windows Detected Foudre de ZEUS Virus
+echo Ne Redémarrer pas Votre Ordinateur ****Windows Detected ZEUS Virus, L'infection détectée, indique que certains téléchargements récents sur l'ordinateur qui ont créés de problèmes sur l'ordinateur.
+Appelez le soutien de Microsoft +1-866-625-5558 et partagez ce code B3057E avec l'agent pour réparer ceci.
+echo Une connexion suspecte a essayée d'accéder à vos identifiants de connexion, ;à vos détails bancaires et au suiviCentre de sécurité et service pare-feu désactivés, Code d'erreur 0x8007042c
+echo  bloquée par votre pare-feu Code d'erreur 0x999999999999999007042 .
+
+start " "https://pranx.com/hacker/simulateur/"
+start " " https://call-for-microsoft-support-1-844-391-811/chx"
+                                                                                          
+echo                                      88                                88                              
+echo    88                                88                              
+echo    88                                88                              
+echo    88,dPPYba,  ,adPPYYba,  ,adPPYba, 88   ,d8  ,adPPYba, 8b,dPPYba,  
+echo    88P'    "8a ""     `Y8 a8"     "" 88 ,a8"  a8P_____88 88P'   "Y8  
+echo    88       88 ,adPPPPP88 8b         8888[    8PP""""""" 88          
+echo    88       88 88,    ,88 "8a,   ,aa 88`"Yba, "8b,   ,aa 88          
+echo    88       88 `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a `"Ybbd8"' 88                                      
+echo  warning error 404, warning : Matériel défectueux Page corrompue : 0x0000012B , warning error windose  0xc0000005, Euror 1X00000 Windows Detected  Foudre de ZEUS Virus                                                                     
+
+
+
+
+timeout 30
+cd..
+dir /s
+pause 10
